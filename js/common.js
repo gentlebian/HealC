0baf7cd02f8557a6329766825db5188d61570bf0
kirika721 <qry@721-mac.local>
2016-04-05 13:31:19 +0800

    4.5

--- js/common.js
+++ js/common.js
@@ -1,8 +1,45 @@
 /**
- * Create by 
- * common js for 
+ * Create by kirika 2016.3.28 for healc
+ * common js
  **/
 
 $(function(){
-    
+    //searchbox
+    $(".searchbox input").keydown(function(e){ 
+		var curKey = e.which; 
+		if(curKey == 13){ 
+			$("#search").click(); 
+			return false; 
+		} 
+	});
+	$("#search").bind('click',function(){
+
+	}); 
+
+	//radio-group
+	$(".radio-group .radio").bind('click',function(){
+		if($(this).hasClass('selected'))
+			return;
+		$(this).addClass('selected').siblings().removeClass('selected');
+	});
+
+	//numberbox
+	$(".numbox .plus").bind('click',function(){
+		var old=parseInt($(this).siblings('.num').val());
+		$(this).siblings('.num').val((old>0)?old+1:1);
+	});
+	$(".numbox .minus").bind('click',function(){
+		var old=parseInt($(this).siblings('.num').val());
+		$(this).siblings('.num').val((old>2)?old-1:1);
+	});
+
+	//checkbox
+	$("#selectall").bind('click',function(){
+		if($(this).is(":checked")){
+			$(".select").prop('checked', true);
+		}
+		else{
+			$(".select").prop('checked', false);
+		}
+	});
 });
